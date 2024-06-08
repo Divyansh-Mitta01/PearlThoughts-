944a0de73253d3760ce232a9c6d4d451694e7ea4
Divyansh Mittal <84392923+Divyansh-Mitta01@users.noreply.github.com>
2024-06-09 00:19:53 +0530

    Update variables.tf

--- terraform-deploy/variables.tf
+++ terraform-deploy/variables.tf
@@ -1,9 +1,4 @@
-variable "key_name" {
-  description = "The name of the key pair to use for the instance"
-  type        = string
-}
-
-variable "private_key_path" {
-  description = "Path to the private key file corresponding to the key pair"
-  type        = string
+variable "region" {
+  description = "The AWS region to deploy to"
+  default     = "us-east-1"
 }
